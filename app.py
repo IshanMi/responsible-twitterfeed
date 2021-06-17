cf5d2d04b3a90ff590a42e3cc51999b4dfc7d24e
Ishan <mishra.ishan@gmail.com>
2021-06-16 23:22:14 -0400

    Can search for Tweets and view results

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from src.feed_creator import get_api, get_tweets
 from dotenv import load_dotenv
 import os
@@ -10,12 +10,37 @@ app.config["API_KEY"] = os.getenv("API_KEY")
 app.config["SECRET_KEY"] = os.getenv("API_SECRET_KEY")
 
 
-@app.route('/')
-@app.route('/home')
-def feed():
+@app.route('/', methods=["GET", "POST"])
+@app.route('/home', methods=["GET", "POST"])
+def get_search_term():
+    if request.method == "GET":
+        return render_template("form.html")
+    else:
+        search_term = request.form["search_term"]
+        quantity = request.form["quantity"]
+
+        # Double check that it's a number
+        if quantity.isdigit():
+            quantity = int(quantity)
+
+            if quantity > 100:
+                quantity = 100
+        else:
+            quantity = 10
+
+        return redirect(url_for('search_results', search_term=search_term, num=quantity))
+
+
+@app.route('/search/<string:search_term>/<int:num>')
+def search_results(search_term, num: int = 20):
+    """ This returns a static result, not a stream. """
+
+    # Authenticate and get the Twitter API
     twitter_api = get_api(api_key=app.config["API_KEY"], secret_key=app.config["SECRET_KEY"])
-    sample_tweets = get_tweets(api=twitter_api, query="PyBites")
-    return render_template("test_api.html", tweets=sample_tweets)
+
+    # Get Tweets based on their search term
+    sample_tweets = get_tweets(api=twitter_api, query=search_term, limit=num)
+    return render_template("search_results.html", tweets=sample_tweets)
 
 
 if __name__ == '__main__':
--- src/feed_creator.py
+++ src/feed_creator.py
@@ -6,8 +6,10 @@ def get_api(api_key, secret_key):
     return tweepy.API(auth)
 
 
-def get_tweets(api: tweepy.API, query: str, limit=10):
-    return [t.text for t in tweepy.Cursor(api.search, q=query).items(limit)]
+def get_tweets(api: tweepy.API, query: str, limit=20):
+    # .encode('ascii', errors='ignore')
+    return [t.full_text for t in
+            tweepy.Cursor(api.search, q=query, tweet_mode='extended').items(limit)]
 
 
 def testing():
